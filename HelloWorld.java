c8718636184149099ba457eebd5cc491f4d12902
Colby McDonald <67612969+Colby-M@users.noreply.github.com>
2020-11-20 13:36:13 -0500

    Both other labs added to this repository
    Added both to the original repository

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+
+/**
+ *
+ * @author cdavi
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello World, this is Colby!");
+    }
+}
---
+++ PaintEstimator.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+import java.lang.Math;     // Note: Needed for math functions in part (3)
+
+public class PaintEstimator {
+   public static void main(String[] args) {
+      Scanner scnr = new Scanner(System.in);
+      double wallHeight;
+      double wallWidth;
+      double wallArea;
+      double paint;
+      int cans;
+      
+      System.out.println("Enter wall height (feet):");
+      wallHeight = scnr.nextDouble();
+      System.out.println("Enter wall width (feet):");
+      wallWidth = scnr.nextDouble();                      // FIXME (1): Prompt user to input wall's width
+      
+      // Calculate and output wall area
+      wallArea = wallWidth * wallHeight;                        // FIXME (1): Calculate the wall's area
+      System.out.println("Wall area: "+ wallArea+ " square feet");     // FIXME (1): Finish the output statement
+      
+      // FIXME (2): Calculate and output the amount of paint in gallons needed to paint the wall
+      paint = wallArea / 350;
+      System.out.println("Paint needed: "+ paint + " gallons");
+      // FIXME (3): Calculate and output the number of 1 gallon cans needed to paint the wall, rounded up to nearest integer
+      cans = (int)Math.ceil(paint);
+      System.out.println("Cans needed: "+ cans + " can(s)");
+   }
+}
